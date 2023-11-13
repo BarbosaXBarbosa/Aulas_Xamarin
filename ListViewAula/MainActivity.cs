6ce3c7a1e8a3aa06896038699377a58661e9a562
João Victor Barbosa <joabarbosa@empro.com.br>
2023-11-13 14:54:19 -0300

    Montando uma interface visual em uma aplicação android.

---
+++ ListViewAula/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace ListViewAula
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.activity_main);
+            EmployeeList employeeList = new EmployeeList();
+            var employees = employeeList.GetEmployees(20);
+
+            ListView lvEmployees = FindViewById<ListView>(id: Resource.Id.lvEmployee);
+            EmployeeAdapter adapter = new EmployeeAdapter(employees);
+            lvEmployees.Adapter = adapter;
+        }
+    }
+}
